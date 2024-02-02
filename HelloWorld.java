0218fd9e313df0b4c36fe3e9836bcc42596769d5
Matthew Chang <mchang3301@gmail.com>
2024-02-01 19:50:45 -0800

    .

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,20 @@
 public class HelloWorld {
 
-    public static void main(String [] args) {
-        System.out.println("Hello, World");
-    }
+    public static void zoop(String insult) {
+        baffle();
+        System.out.print(“You “ + insult);
+      }
+      
+      public static void main(String[] args) {
+        System.out.print(“No, I “);
+        zoop(“wugga!”);
+        System.out.print(“I “);
+        baffle();
+      } 
+      
+      public static void baffle() {
+        int x = 5;
+        System.out.print(“don’t, you do!”)
+      }
+      
 }
